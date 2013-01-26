9896b24f77fe0cac46c0f8daf9f9e954981544de
Alessandro Franceschi <al@lab42.it>
2013-01-26 23:41:40 +0100

    Fix for https://github.com/example42/puppet-jboss/issues/6 and https://github.com/example42/puppet-jboss/issues/5

--- manifests/user.pp
+++ manifests/user.pp
@@ -18,7 +18,14 @@ class jboss::user inherits jboss {
     home       => $jboss::real_jboss_dir,
     shell      => '/bin/bash',
   }
-
   User <| title == $jboss::process_user |>
+
+  if $jboss::user_gid {
+    @group { $jboss::process_user :
+      ensure     => $jboss::manage_file,
+      gid        => $jboss::user_gid,
+    }
+    Group <| title == $jboss::process_user |>
+  }
 
 }
