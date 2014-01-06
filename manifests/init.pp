3a44a5e06ac5f1b00ce122a866e5a924837f3029
Joshua Hoblitt <jhoblitt@cpan.org>
2014-01-06 13:27:01 -0700

    change jboss 4 and 5 download URLs to work with puppi::netinstall
    Fix per discussion in #15

--- manifests/init.pp
+++ manifests/init.pp
@@ -423,8 +423,8 @@ class jboss (
 
   $real_install_source = $jboss::install_source ? {
     ''      => $jboss::version ? {
-      '4' => 'http://sourceforge.net/projects/jboss/files/JBoss/JBoss-4.2.3.GA/jboss-4.2.3.GA.zip/download',
-      '5' => 'http://sourceforge.net/projects/jboss/files/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA.zip/download',
+      '4' => 'http://sourceforge.net/projects/jboss/files/JBoss/JBoss-4.2.3.GA/jboss-4.2.3.GA.zip',
+      '5' => 'http://sourceforge.net/projects/jboss/files/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA.zip',
       '6' => 'http://download.jboss.org/jbossas/6.1/jboss-as-distribution-6.1.0.Final.zip',
       '7' => 'http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip',
     },
