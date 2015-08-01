80b57677875e2be7bde33970dcfb8efafd13209a
Martin Alfke <ma@example42.com>
2015-08-01 21:13:52 +0200

    work on rspec tests
    - pin rspec to 3.1.x on ruby 1.8
    - needs puppetlabs/stdlib version 4.0.0
    - rspec deprecations

--- spec/classes/jboss_spec.rb
+++ spec/classes/jboss_spec.rb
@@ -1,10 +1,14 @@
-require "#{File.join(File.dirname(__FILE__),'..','spec_helper.rb')}"
+require 'spec_helper'
 
 describe 'jboss' do
 
   let(:title) { 'jboss' }
   let(:node) { 'rspec.example42.com' }
-  let(:facts) { { :ipaddress => '10.42.42.42' , :operatingsystem => 'RedHat' } }
+  let(:facts) { {
+      :ipaddress => '10.42.42.42',
+      :operatingsystem => 'RedHat',
+      :concat_basedir => '/dne'
+  } }
 
   describe 'Test standard installation via package' do
     let(:params) { {:install => 'package' } }
@@ -15,16 +19,14 @@ describe 'jboss' do
   describe 'Test installation via netinstall' do
     let(:params) { {:version => '6' } }
     it 'should install version 6 via netinstall' do
-      content = catalogue.resource('puppi::netinstall', 'netinstall_jboss').send(:parameters)[:url]
-      content.should match "http://download.jboss.org/jbossas/6.1/jboss-as-distribution-6.1.0.Final.zip"
+      should contain_puppi__netinstall('netinstall_jboss').with_url("http://download.jboss.org/jbossas/6.1/jboss-as-distribution-6.1.0.Final.zip")
     end
   end
 
   describe 'Test installation via puppi' do
     let(:params) { {:version => '6' , :install => 'puppi' } }
     it 'should install version 6 via puppi' do
-      content = catalogue.resource('puppi::project::archive', 'jboss').send(:parameters)[:source]
-      content.should match "http://download.jboss.org/jbossas/6.1/jboss-as-distribution-6.1.0.Final.zip"
+      should contain_puppi__project__archive('jboss').with_source("http://download.jboss.org/jbossas/6.1/jboss-as-distribution-6.1.0.Final.zip")
     end
   end
 
@@ -33,12 +35,10 @@ describe 'jboss' do
 
     it { should contain_package('jboss').with_ensure('present') }
     it 'should monitor the process' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('jboss_process').with_enable(true)
     end
     it 'should place a firewall rule' do
-      content = catalogue.resource('firewall', 'jboss_tcp_42').send(:parameters)[:enable]
-      content.should == true
+      should contain_firewall('jboss_tcp_42').with_enable(true)
     end
   end
 
@@ -48,12 +48,10 @@ describe 'jboss' do
     it 'should remove Package[jboss]' do should contain_package('jboss').with_ensure('absent') end 
     it 'should not enable at boot Service[jboss]' do should contain_service('jboss').with_enable('false') end
     it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('jboss_process').with_enable(false)
     end
     it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'jboss_tcp_42').send(:parameters)[:enable]
-      content.should == false
+      should contain_firewall('jboss_tcp_42').with_enable(false)
     end
   end
 
@@ -62,12 +60,10 @@ describe 'jboss' do
 
     it { should contain_package('jboss').with_ensure('present') }
     it 'should not monitor the process' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('jboss_process').with_enable(false)
     end
     it 'should remove a firewall rule' do
-      content = catalogue.resource('firewall', 'jboss_tcp_42').send(:parameters)[:enable]
-      content.should == false
+      should contain_firewall('jboss_tcp_42').with_enable(false)
     end
   end
 
@@ -77,12 +73,10 @@ describe 'jboss' do
     it { should contain_package('jboss').with_ensure('present') }
     it 'should not enable at boot Service[jboss]' do should contain_service('jboss').with_enable('false') end
     it 'should not monitor the process locally' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == false
+      should contain_monitor__process('jboss_process').with_enable(false)
     end
     it 'should keep a firewall rule' do
-      content = catalogue.resource('firewall', 'jboss_tcp_42').send(:parameters)[:enable]
-      content.should == true
+      should contain_firewall('jboss_tcp_42').with_enable(true)
     end
   end 
 
@@ -90,12 +84,10 @@ describe 'jboss' do
     let(:params) { {:template => "jboss/spec.erb" , :options => { 'opt_a' => 'value_a' } } }
 
     it 'should generate a valid template' do
-      content = catalogue.resource('file', 'jboss.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('jboss.conf').with_content(/fqdn: rspec.example42.com/)
     end
     it 'should generate a template that uses custom options' do
-      content = catalogue.resource('file', 'jboss.conf').send(:parameters)[:content]
-      content.should match "value_a"
+      should contain_file('jboss.conf').with_content(/value_a/)
     end
 
   end
@@ -104,24 +96,20 @@ describe 'jboss' do
     let(:params) { {:source => "puppet://modules/jboss/spec" , :source_dir => "puppet://modules/jboss/dir/spec" , :source_dir_purge => true } }
 
     it 'should request a valid source ' do
-      content = catalogue.resource('file', 'jboss.conf').send(:parameters)[:source]
-      content.should == "puppet://modules/jboss/spec"
+      should contain_file('jboss.conf').with_source("puppet://modules/jboss/spec")
     end
     it 'should request a valid source dir' do
-      content = catalogue.resource('file', 'jboss.dir').send(:parameters)[:source]
-      content.should == "puppet://modules/jboss/dir/spec"
+      should contain_file('jboss.dir').with_source("puppet://modules/jboss/dir/spec")
     end
     it 'should purge source dir if source_dir_purge is true' do
-      content = catalogue.resource('file', 'jboss.dir').send(:parameters)[:purge]
-      content.should == true
+      should contain_file('jboss.dir').with_purge(true)
     end
   end
 
   describe 'Test customizations - custom class' do
     let(:params) { {:my_class => "jboss::spec" , :template => "jboss/spec.erb"} }
     it 'should automatically include a custom class' do
-      content = catalogue.resource('file', 'jboss.conf').send(:parameters)[:content]
-      content.should match "fqdn: rspec.example42.com"
+      should contain_file('jboss.conf').with_content(/fqdn: rspec.example42.com/)
     end
   end
 
@@ -129,8 +117,7 @@ describe 'jboss' do
     let(:params) { {:puppi => true, :puppi_helper => "myhelper"} }
 
     it 'should generate a puppi::ze define' do
-      content = catalogue.resource('puppi::ze', 'jboss').send(:parameters)[:helper]
-      content.should == "myhelper"
+      should contain_puppi__ze('jboss').with_helper("myhelper")
     end
   end
 
@@ -138,8 +125,7 @@ describe 'jboss' do
     let(:params) { {:monitor => true, :monitor_tool => "puppi" } }
 
     it 'should generate monitor defines' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:tool]
-      content.should == "puppi"
+      should contain_monitor__process('jboss_process').with_tool("puppi")
     end
   end
 
@@ -147,8 +133,7 @@ describe 'jboss' do
     let(:params) { {:firewall => true, :firewall_tool => "iptables" , :protocol => "tcp" , :port => "42" } }
 
     it 'should generate correct firewall define' do
-      content = catalogue.resource('firewall', 'jboss_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
+      should contain_firewall('jboss_tcp_42').with_tool("iptables")
     end
   end
 
@@ -156,16 +141,13 @@ describe 'jboss' do
     let(:params) { {:monitor => "yes" , :monitor_tool => "puppi" , :firewall => "yes" , :firewall_tool => "iptables" , :puppi => "yes" , :port => "42" , :protocol => 'tcp' } }
 
     it 'should generate monitor resources' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:tool]
-      content.should == "puppi"
+      should contain_monitor__process('jboss_process').with_tool("puppi")
     end
     it 'should generate firewall resources' do
-      content = catalogue.resource('firewall', 'jboss_tcp_42').send(:parameters)[:tool]
-      content.should == "iptables"
+      should contain_firewall('jboss_tcp_42').with_tool("iptables")
     end
     it 'should generate puppi resources ' do 
-      content = catalogue.resource('puppi::ze', 'jboss').send(:parameters)[:ensure]
-      content.should == "present"
+      should contain_puppi__ze('jboss').with_ensure("present")
     end
   end
 
@@ -174,8 +156,7 @@ describe 'jboss' do
     let(:params) { { :port => '42' } }
 
     it 'should honour top scope global vars' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('jboss_process').with_enable(true)
     end
   end
 
@@ -184,8 +165,7 @@ describe 'jboss' do
     let(:params) { { :port => '42' } }
 
     it 'should honour module specific vars' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('jboss_process').with_enable(true)
     end
   end
 
@@ -194,8 +174,7 @@ describe 'jboss' do
     let(:params) { { :port => '42' } }
 
     it 'should honour top scope module specific over global vars' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('jboss_process').with_enable(true)
     end
   end
 
@@ -204,8 +183,7 @@ describe 'jboss' do
     let(:params) { { :monitor => true , :firewall => true, :port => '42' } }
 
     it 'should honour passed params over global vars' do
-      content = catalogue.resource('monitor::process', 'jboss_process').send(:parameters)[:enable]
-      content.should == true
+      should contain_monitor__process('jboss_process').with_enable(true)
     end
   end
 
